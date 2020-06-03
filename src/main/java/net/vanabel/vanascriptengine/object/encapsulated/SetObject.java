e013e488fff38cfe0d86ef70796d4d211c2b8354
VangNgo <vang.nam.ngo@gmail.com>
2020-06-03 03:15:08 -0400

    Improve SetObject class
    Add prelimiary attributes
    
    Add rudimentary type ensurance
    
    TODO: Remove generics from this scripting language, or allow them?

--- src/main/java/net/vanabel/vanascriptengine/object/encapsulated/SetObject.java
+++ src/main/java/net/vanabel/vanascriptengine/object/encapsulated/SetObject.java
@@ -1,8 +1,12 @@
 package net.vanabel.vanascriptengine.object.encapsulated;
 
+import net.vanabel.vanascriptengine.attribute.Attribute;
 import net.vanabel.vanascriptengine.object.AbstractObject;
+import net.vanabel.vanascriptengine.object.ObjectRegistry;
 import net.vanabel.vanascriptengine.object.annotation.ObjectConstructor;
 import net.vanabel.vanascriptengine.object.annotation.ObjectMatcher;
+import net.vanabel.vanascriptengine.object.datatype.BooleanDataType;
+import net.vanabel.vanascriptengine.object.datatype.IntegerDataType;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -10,6 +14,43 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class SetObject<T extends AbstractObject> extends EncapsulatedObject implements Set<T> {
+
+    public final static AttributeHandler<SetObject<AbstractObject>> ATTRIBUTE_HANDLER = new AttributeHandler<>();
+
+    public static void registerAttributes(Attribute.Processor<SetObject<AbstractObject>> processor, String... names) {
+        ATTRIBUTE_HANDLER.registerAttributes(processor, names);
+    }
+
+    static {
+        registerAttributes(
+                (Attribute.DirectProcessor<SetObject<AbstractObject>>) (object, attribute) -> new IntegerDataType(object.size()),
+                "size"
+        );
+        registerAttributes(
+                (Attribute.DirectProcessor<SetObject<AbstractObject>>) (object, attribute) -> BooleanDataType.getForBoolean(object.isEmpty()),
+                "is_empty", "isEmpty"
+        );
+        registerAttributes(
+                (Attribute.DirectProcessor<SetObject<AbstractObject>>) (object, attribute) -> {
+                    if (!attribute.hasContext()) {
+                        // TODO: Debug
+                        return BooleanDataType.getForBoolean(false);
+                    }
+                    // TODO: This
+                    return null;
+                }, "contains"
+        );
+        registerAttributes(
+                (object, attribute) -> {
+                    if (!attribute.hasContext()) {
+                        // TODO: Debug
+                        return null;
+                    }
+                    object.add(ObjectRegistry.constructForClass(attribute.getContext().getRaw(), object.clss));
+                    return object;
+                }, "add"
+        );
+    }
 
     @ObjectConstructor
     public static <S extends AbstractObject> SetObject<S> construct(String value) {
@@ -23,14 +64,22 @@ public class SetObject<T extends AbstractObject> extends EncapsulatedObject impl
         return false;
     }
 
-    private Set<T> data;
+    private final Class<T> clss;
+    private final Set<T> data;
 
     public SetObject() {
         this(new HashSet<>());
     }
 
+    @SuppressWarnings("unchecked")
     public SetObject(Set<T> set) {
         data = set;
+        try {
+            clss = (Class<T>) Class.forName(getClass().getField("data").getGenericType().getTypeName());
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Unexpected error while constructing a SetObject!");
+        }
     }
 
     @Override
